9465df6b774cd46cbeb31b28f2ef983e4a75ca5f
_jordaj3 <juangui.jordan2@takeda.com>
2021-07-31 17:05:54 +0200

    Static build and Netlify config

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("src/db/db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
